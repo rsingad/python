258babb1ff976460ec24ad64fd09c2f9bfe2583c
rsingad <rsingerjat@gmail.com>
2025-03-17 14:53:25 +0530

    add python 100 h

---
+++ add.py
@@ -0,0 +1,55 @@
+# print("addition program")
+# a=int(input("enter the value of a="));
+# b=int(input("enter the value of b:"));
+# print(a+b);
+
+
+
+# print(a<b);
+
+
+
+
+
+
+
+# per=float(input("enter prsenteg:"));
+# city=str(input("enter city name:"));
+# city=print(input("enter city name:"));
+# per=print(int(input("enter prsenteg:")));
+
+
+# print(per ," \t",city)
+
+
+
+
+
+# assignment operator
+# a+=b;
+# a=b;
+# a-=b;
+
+
+
+
+# member ship operator
+
+l=[1,2,3,4,5]
+print(l);
+
+# 1 .in 
+# 2 .NOT IN 
+print(2 in l);
+print(10 in l)
+print(10 not in l);
+
+
+
+# identity operator
+print("\t identity operator")
+# 1. is 
+# 2. is not
+print(10 is l);
+print(2 is not l);
+
---
+++ day4.py
@@ -0,0 +1,74 @@
+str1="ceg";
+str2="jaipur";
+
+str_out=str1+str2;
+print(str_out);
+str_out2="krishna"+"choudhary";
+print(str_out2);
+
+
+# task1
+# python is awersome{print using string concate method}
+result="Python"+"is"+"awersome";
+print(result);
+
+# concate two numbers (1 and 2)
+num_Sum=1+2;
+print(num_Sum);
+num_Concate="1"+"2";
+print(num_Concate);
+
+
+# string repeation(*)
+rep_1="kannha||" * 3;
+print(rep_1);
+
+#string index(start 0 index)(ending n-1)
+name="vikaram";
+print("indexing opsitive||index no 1=> char:",name[1]);
+print("indexing opositive");
+
+
+# 
+str3="programming";
+# outputSlice=str3.slice(0,4);
+outputSlice=str3[0:4];#str_name[,staring_index, ending_index]//ending_index exclude
+print(outputSlice);#pro
+print(str3[::2]);#skip 1 char
+print(str3[::-1]);#reverse string
+str4=str3.upper();# lower()//
+print("uper() method conver uppercase string=>",str3,"=>",str4);
+
+str6="python Programming";
+
+sub_str6=str6[0:6];
+print(sub_str6);
+print(str6[-11:]);#auto end point
+
+
+
+
+#1. conver in full string
+task1="coding";
+task2="is";
+task3="fun";
+res3=task1+task2+task3;
+print(task1+task2+task3);
+
+#python
+text_length=len(res3);
+print(text_length);
+
+#
+str11="example";
+len_str11=len(str11);
+print("length of 'example'=>",len_str11);
+
+# numberAsString="123456789";
+# print(numberAsString);
+
+
+
+
+
+
