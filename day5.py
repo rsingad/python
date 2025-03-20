ea7305fe328c5b7ebc75ce324e5f6ee0a7dc3dad
rsingad <rsingerjat@gmail.com>
2025-03-20 14:55:07 +0530

    add day 4 program task and asignment

---
+++ day5.py
@@ -0,0 +1,54 @@
+month=str(input("enter month name="));
+# if month in ("october","nov","des"):
+#     print("seet reetu");
+# elif month in ("jan","feb","march"):
+#     print("xys");
+# elif month in ("april","may","june"):
+#     print("grism reetu");
+# elif month in ("july","august","september"):
+#     print("vasnt reetu");
+
+
+# task2
+# if "a" in month.lower():
+#     print("conver:");
+# else:
+#     print("XXXXXXXXXXXXXXXXX");
+
+# print(month);
+
+
+# task 3
+"""
+30 =>hot 
+20=> warm
+10 => colg
+"""
+
+
+#task 6
+#  aeiou ,AEIOU =>vovalfountand not fount
+# text=input("enyte char=");
+# voval="a","e","i","o","u","A","E","I","O","U";
+# if text in voval:
+#     print("entered char is voval:");
+# else:
+#     print("enterd char is not voval");
+
+# task 7
+# positive and even odd
+numX=int(input("enter a number:"));
+if (numX>0):
+    if(numX%2==0):
+        print(numX ," is even and positive");
+    else:
+        print(numX," is odd and positive");
+else:
+    print(numX," is not negative");
+
+# user verifiction 
+# logged in=>true 
+# is_admin=>true
+# log in and admin hone pr log in admint print kre
+# use log but not admin
+# user not login
