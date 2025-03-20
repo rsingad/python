7553c7468cea3f5c7a2bbcc96ba390d6e1a5d7d8
rsingad <rsingerjat@gmail.com>
2025-03-20 14:59:05 +0530

    update all program

---
+++ ifelse.py
@@ -0,0 +1,70 @@
+# 19 march 
+# online vote system conditions....
+# a=int(input("enter the voter age="));
+# if (a>=18):
+#     print("voter eligaable");
+# else:
+#     print("not eligable");
+
+# input 2value and and chaking which one id grater
+
+# num1=int(input("enter value a="));
+# num2=int(input("enter value b="));
+
+# if (num1>=num2):
+#     print(num1,"is grater then ",num2);
+# else:
+#     print(num2," is grater then ",num1);
+
+# task 3
+#enter the weeak number uusing if elif print weekname
+
+# weekNo=int(input("enter week no:"));
+
+# if(weekNo==1):
+#     print("today is sunday");
+# elif(weekNo==2):
+#     print("today is monday");
+# elif(weekNo==3):
+#     print("today is tuesday");
+# elif(weekNo==4):
+#     print("today is wednesday");
+# elif(weekNo==5):
+#     print("today is thrusday");
+# elif(weekNo==6):
+#     print("today is friday");
+# elif(weekNo==7):
+#     print("today is saturday");
+# else:
+#     print("plzz enter valid week number ");
+
+
+# task 4
+# cheking number even and odd
+# numX=int(input("enter the number:"));
+# if(numX%2==0):
+#     print(numX," is even");
+# elif(numX%2!=0):
+#     print(numX," is odd");
+
+
+# # task 6
+
+# persent=int(input("enter the student persent:"));
+# if(persent>60):
+#     print("student devsion:first");
+# elif(persent>45):
+#     print("student devsion:second");
+# elif(persent>36):
+#     print("student devsion:third");
+# else:
+#     print("student is failllllllllllllllll");
+
+
+# 7 ) 
+# grater then 500 => discount 5% and grater then 1000 => dicount 10% , garater then 2000 =>discount 20%
+# print((500*20)/100);
+buy_amount=int(input(" enter shoping amount="));
+if(buy_amount<=500):
+    discount =5;
+    print("shoping amount:",buy_amount,"||discount 5%","|| total amount=",(buy_amount*discount)/100);
